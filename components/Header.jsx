ed5687a50b144fa17ad4973aa252fae339c274e3
Joyel <Varghesejoyel71@gmail.com>
2025-05-16 15:49:11 +0530

    Resume updated

--- components/Header.jsx
+++ components/Header.jsx
@@ -47,7 +47,7 @@ const Header = () => {
             initial={{y: 30, opacity: 0}}
             whileInView={{y: 0, opacity: 1}}
             transition={{duration: 0.6, delay: 1.2}}
-            href="/resume.pdf" download 
+            href="/Joyel_Varghese_Resume_Full Stack Developer.pdf" download 
             className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>
                 My Resume <Image src={assets.download_icon} alt='' className='w-4'/></motion.a>
         </div>
